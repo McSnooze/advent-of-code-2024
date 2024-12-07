b941744b2ca1fb2fc86acdcb5090b95a46d55f02
Steve Sternitzke <steve.sternitzke@enviam.de>
2024-12-07 16:55:29 +0100

    add day 7

---
+++ 07_02.ts
@@ -0,0 +1,25 @@
+import {input} from "./07.input.ts";
+
+
+const calc = (search: number, current: number, [next, ...rest]: number[])=>{
+    if (next === undefined) {
+        return current === search;
+    }
+
+    if (current > search) return false;
+
+    return calc(search, current + next, rest)
+        || calc(search, current * next, rest)
+        || calc(search, Number(`${current}${next}`), rest);
+}
+
+const sum = input.trim().split("\n").map((line) => {
+    const [result, rest] = line.split(": ");
+    const [first, ...list] = rest.split(" ").map(Number);
+    const search = Number(result);
+
+    return calc(search, first, list) ? search : 0;
+}).reduce((acc, value) => acc + value, 0);
+
+console.log(sum);
+
